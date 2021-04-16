052b80b5d85c14649e5f7d92816e81912b69dc2e
Dmytro Rizdvanetskyi <reedrefucher@gmail.com>
2021-04-16 22:33:19 +0300

    functions to connect to postgresql

---
+++ backendService/dbmodule.py
@@ -0,0 +1,38 @@
+import psycopg2
+
+def connect(params_dic):
+    """ Connect to the PostgreSQL database server """
+    conn = None
+    try:
+        # connect to the PostgreSQL server
+        print('Connecting to the PostgreSQL database...')
+        conn = psycopg2.connect(**params_dic)
+    except (Exception, psycopg2.DatabaseError) as error:
+        print(error)
+        sys.exit(1) 
+    print("Connection successful")
+    return conn
+
+def execute_many(conn, df, table):
+    """
+    Using cursor.executemany() to insert the dataframe
+    """
+    # Create a list of tupples from the dataframe values
+    tuples = [tuple(x) for x in df.to_numpy()]
+    # Comma-separated dataframe columns
+    cols = ','.join(list(df.columns))
+    # SQL quert to execute
+    #query  = "INSERT INTO %s(%s)" % (table, cols) + "VALUES(%%s, %%s, %%s)"
+    query  = "INSERT INTO %s(%s)" % (table, cols) + "VALUES(" + ",".join(["%s"]*len(list(df.columns))) + ")"
+    cursor = conn.cursor()
+    try:
+        cursor.executemany(query, tuples)
+        conn.commit()
+    except (Exception, psycopg2.DatabaseError) as error:
+        print("Error: %s" % error)
+        conn.rollback()
+        cursor.close()
+        return 1
+    print("execute_many() done")
+    cursor.close()
+
